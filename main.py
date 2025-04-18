a50b834c91c51f6a7ff03ef42d8d53ad029d71dd
babakavizesh <babak.avizeshm@gmail.com>
2025-04-19 03:03:07 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from telethon.sync import TelegramClient
+from telethon import functions
+from datetime import datetime
+import time
+import os
+
+# اطلاعات تلگرام خودتو اینجا وارد کن
+api_id = int(os.environ.get("383244"))
+api_hash = os.environ.get("0abb6c73099af28084bfa507e6f26191
+")
+
+client = TelegramClient('session', api_id, api_hash)
+
+with client:
+    while True:
+        now = datetime.now().strftime('%H:%M')
+        name = f"Ali | {now}"  # ← اینجا اسمتو بذار
+        client(functions.account.UpdateProfileRequest(first_name=name))
+        print(f"نام بروزرسانی شد: {name}")
+        time.sleep(60)
