433db9433d557d5aa47f85245251f6dae6c8041d
babakavizesh <babak.avizeshm@gmail.com>
2025-04-19 04:16:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ import time
 import os
 import pytz
 
+# اطلاعات api_id و api_hash از متغیرهای محیطی
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 
-client = TelegramClient('session', api_id, api_hash)
-
-# منطقه زمانی ایران
+# منطقه زمانی ایران (تهران)
 iran_timezone = pytz.timezone("Asia/Tehran")
+
+# اتصال به تلگرام
+client = TelegramClient('session', api_id, api_hash)
 
 with client:
     while True:
+        # دریافت زمان جاری به وقت ایران
         now = datetime.now(iran_timezone).strftime('%H:%M')
-        name = f"BbK | {now}"  # ← اسم دلخواهت
+        name = f"BbK | {now}"  # ← اینجا می‌تونی اسم دلخواهت رو بذاری
         client(functions.account.UpdateProfileRequest(first_name=name))
         print(f"نام بروزرسانی شد: {name}")
-        time.sleep(60)
+        time.sleep(60)  # هر دقیقه یک بار
