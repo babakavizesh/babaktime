cdbb54e1cac2e56dbf7211197569c3f90c1b366d
babakavizesh <babak.avizeshm@gmail.com>
2025-04-19 04:10:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from telethon import functions
 from datetime import datetime
 import time
 import os
+import pytz
 
-# دریافت api_id و api_hash از متغیرهای محیطی
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 
-# مقدار داخل پرانتز باید دقیقاً همون اسم فایل session بدون پسوند باشه
 client = TelegramClient('session', api_id, api_hash)
+
+# منطقه زمانی ایران
+iran_timezone = pytz.timezone("Asia/Tehran")
 
 with client:
     while True:
-        now = datetime.now().strftime('%H:%M')
-        name = f"BbK | {now}"  # ← اینجا اسمت رو می‌تونی عوض کنی
+        now = datetime.now(iran_timezone).strftime('%H:%M')
+        name = f"BbK | {now}"  # ← اسم دلخواهت
         client(functions.account.UpdateProfileRequest(first_name=name))
         print(f"نام بروزرسانی شد: {name}")
         time.sleep(60)
