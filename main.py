64e40caa75c6e7a1b99b649065a94e263981ee4e
babakavizesh <babak.avizeshm@gmail.com>
2025-04-19 04:06:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ from datetime import datetime
 import time
 import os
 
-# اطلاعات تلگرام خودتو اینجا وارد کن
-api_id = int(os.environ.get("API_ID"))  # نام صحیح متغیر محیطی رو وارد کن
-api_hash = os.environ.get("API_HASH")   # نام صحیح متغیر محیطی رو وارد کن
+# دریافت api_id و api_hash از متغیرهای محیطی
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
 
-# بررسی برای اطمینان از وجود مقادیر
-if not api_id or not api_hash:
-    raise ValueError("API_ID or API_HASH is not set properly in environment variables.")
-
+# مقدار داخل پرانتز باید دقیقاً همون اسم فایل session بدون پسوند باشه
 client = TelegramClient('session', api_id, api_hash)
 
 with client:
     while True:
         now = datetime.now().strftime('%H:%M')
-        name = f"BbK | {now}"  # ← اینجا اسمتو بذار
+        name = f"BbK | {now}"  # ← اینجا اسمت رو می‌تونی عوض کنی
         client(functions.account.UpdateProfileRequest(first_name=name))
         print(f"نام بروزرسانی شد: {name}")
         time.sleep(60)
